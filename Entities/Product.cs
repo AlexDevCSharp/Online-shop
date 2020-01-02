0896c28af658f7e27f348ba0bc765358aeb68e37
AlexDevCSharp <56534599+AlexDevCSharp@users.noreply.github.com>
2020-01-02 12:32:09 +0200

    Add files via upload

---
+++ Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace internet_shop.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int BrandId { get; set; }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+    }
+}
